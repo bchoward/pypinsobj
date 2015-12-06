405d0a93bd2f5767d445c49b1708906f6917b06d
bchoward <bchoward@gmail.com>
2015-12-05 20:21:30 -0800

    added real setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,26 @@
- 
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+requires = [
+    'RPi',
+    ]
+
+setup(name='pypinsobj',
+      version='0.1',
+      description='wrapper for RPi',
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      author='bchoward',
+      author_email='',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      entry_points="""\
+      """,
+      )
+
