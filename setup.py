3d7f08f9008442829d57882e84a82056359c736c
bchoward <bchoward@gmail.com>
2015-12-05 20:32:36 -0800

    xx

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 here = os.path.abspath(os.path.dirname(__file__))
 
 requires = [
-    'python-rpi.gpio',
+    'rpi.gpio',
     ]
 
 setup(name='pypinsobj',
